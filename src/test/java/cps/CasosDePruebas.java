57ddfca32562bae2a09c80b032f124bf3cd1dac5
Ronald <ronaldveasm@gmail.com>
2024-01-09 21:32:26 -0300

    Actializacion de repocitorio: Continuacion de contruccion

--- src/test/java/cps/CasosDePruebas.java
+++ src/test/java/cps/CasosDePruebas.java
@@ -41,7 +41,7 @@ public class CasosDePruebas {
     }
 
     @Test
-    public void CP001_inicio_de_sesion() throws InterruptedException {
+    public void CP001_inicio_de_sesion_fallido() throws InterruptedException {
         By localizadorInicioSesion = By.xpath("//span[contains(text(),'Mi cuenta')]");
         WebElement btnMiCuenta = driver.findElement(localizadorInicioSesion);
         btnMiCuenta.click();
@@ -51,7 +51,7 @@ public class CasosDePruebas {
         driver.findElement(By.id("rutId2_uno")).sendKeys("123hjk");
         Thread.sleep(2000);
         driver.findElement(By.id("btn_login_uno")).click();
-        WebElement mensaje = driver.findElement(By.xpath("//div[contains(text(),'RUT o contraseña incorrecta.')]"));
+        String mensaje = driver.findElement(By.xpath("//div[contains(text(),'RUT o contraseña incorrecta.')]")).getText();
 
         Assertions.assertEquals("RUT o contraseña incorrecta.",mensaje);
     }
@@ -71,85 +71,77 @@ public class CasosDePruebas {
         js.executeScript("window.scrollBy(0,1400)");
         Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div[1]/table[1]/tbody/tr[21]/td/a")).click();
+
+        Assertions.assertNotEquals(driver.getCurrentUrl(),"https://www.condorito.cl");
     }
 
     @Test
-    public void CP003(){
+    public void CP003() throws InterruptedException {
+        driver.get("https://www.deezer.com/es/offers");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//button[@id='gdpr-btn-accept-all']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("topbar-login-button")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.id("login_mail")).sendKeys("tsanchez@outlook.com");
+        Thread.sleep(1500);
+        driver.findElement(By.id("login_password")).sendKeys("2DKx91w.o");
+        Thread.sleep(3800);
+
+        driver.findElement(By.id("login_form_submit")).click();
+
+        WebElement mensaje = driver.findElement(By.id("//h1[contains(text(),'Una experiencia única, solo en Deezer')]"));
+        //Select select = new Select(identidad);
+        //select.selectByIndex(2)
+    }
+
+    @Test
+    public void CP004_seguimiento_fallido() throws InterruptedException {
+        driver.get("https://www.tricot.cl/");
+        WebElement btnSeguimiento = driver.findElement(By.xpath("//div[contains(text(),'Seguimiento')]"));
+        btnSeguimiento.click();
+
+        driver.findElement(By.id("invoice")).sendKeys("8768928433");
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//button[contains(text(),'Continuar con el proceso')]")).click();
 
     }
 
     @Test
-    public void CP004(){
-
-    }
-
-    @Test
-    public void  CP005(){
+    public void CP005_compra() throws InterruptedException {
         driver.get("https://www.demoblaze.com/");
         driver.manage().window().maximize();
-        js.executeScript("window.scrollBy(0,4000)");
+
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(2000);
         driver.findElement(By.id("next2")).click();
+
         driver.findElement(By.xpath("//a[contains(text(),'MacBook Pro')]")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Add to cart')]")).click();
-        //Thread.sleep(2500);
+        Thread.sleep(2500);
         driver.switchTo().alert().accept();
         //Thread.sleep(1000);
         driver.findElement(By.id("cartur")).click();
-        //Thread.sleep(1000);
+
         driver.findElement(By.xpath("//button[contains(text(),'Place Order')]")).click();
         //Thread.sleep(1000);
-        driver.findElement(By.id("name")).sendKeys("Carlos");
-        //Thread.sleep(1000);
+        driver.findElement(By.id("name")).sendKeys("Carlos Mendez");
+        Thread.sleep(1000);
         driver.findElement(By.id("country")).sendKeys("Chile");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("city")).sendKeys("Santiago");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("card")).sendKeys("5490561184212290");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("month")).sendKeys("12");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.id("year")).sendKeys("2032");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[contains(text(),'Purchase')]")).click();
-        ////h2[contains(text(),'Thank you for your purchase!')]
+        Thread.sleep(10000);
+        String mensajeCompra = driver.findElement(By.xpath("//h2[contains(text(),'Thank you for your purchase!')]")).getText();
+        Assertions.assertEquals("'Thank you for your purchase!",mensajeCompra);
+
     }
-    /*
-driver.get("https://www.pcfactory.cl/");
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//span[contains(text(),'Mi cuenta')]")).click();
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("//a[contains(text(),'¿No tienes cuenta? Regístrate')]")).click();
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.name("id_nombre_man_cliente")).sendKeys("Tomas Sanchez");
-        driver.findElement(By.id("phone")).sendKeys("987578291");
-        driver.findElement(By.id("id_email_man_cliente")).sendKeys("tsanchez@outlook.com");
-        driver.findElement(By.id("autocomplete")).sendKeys("Las Lilas 931");
-        driver.findElement(By.id("nombre_direccion")).sendKeys("Casa");
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.id("id_rut_man_cliente")).sendKeys("26810643-k");
-        driver.findElement(By.xpath("//button[contains(text(),'Guardar')]")).click();
-
-
-        driver.get("https://www.correos.cl/");
-        driver.manage().window().maximize();
-        Thread.sleep(1500);
-        js.executeScript("window.scrollBy(0,300)");
-        driver.findElement(By.xpath("//body/div[@id='senna_surface1-default']/div[@id='wrapper']/section[@id='content']/div[@id='main-content']/div[1]/div[1]/div[1]/div[3]/section[1]/div[1]/div[2]/div[1]/section[1]/div[1]/div[1]/div[1]/label[2]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("comuna_domicilio")).sendKeys("Santiago");
-        driver.findElement(By.id("//body/div[@id='senna_surface1-default']/div[@id='wrapper']/section[@id='content']/div[@id='main-content']/div[1]/div[1]/div[1]/div[3]/section[1]/div[1]/div[2]/div[1]/section[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[1]/div[1]")).submit();
-    Thread.sleep(5000);
-        driver.findElement(By.id("js_btn_cookie")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("calle_domicilio")).sendKeys("Arturo Prat");
-        Thread.sleep(1500);
-        driver.findElement(By.id("numero_calle_domicilio")).sendKeys("300");
-        Thread.sleep(4000);
-        driver.findElement(By.id("btnCodigoPostal")).click();
-     */
 }
